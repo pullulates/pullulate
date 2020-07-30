20a55df3858e79a35b01836b87d2559e8f7839d6
pullulates <2788913923@qq.com>
2020-07-30 22:30:16 +0800

    地区信息

---
+++ src/main/java/top/pullulate/system/entity/PulDistrict.java
@@ -0,0 +1,53 @@
+package top.pullulate.system.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @功能描述:   地区实体类
+ * @Author: xuyong
+ * @Date: 2020/7/30 22:07
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+@Data
+public class PulDistrict implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /** 地区主键 */
+    @TableId
+    private String districtId;
+
+    /** 上级地区主键 */
+    private String parentId;
+
+    /** 行政区名称 */
+    private String name;
+
+    /** 行政区划级别 */
+    private String level;
+
+    /** 城市编码 */
+    private String citycode;
+
+    /** 区域编码 */
+    private String adcode;
+
+    /** 区域中心点 */
+    private String center;
+
+    /** 行政区边界坐标点 */
+    private String polyline;
+
+    /** 创建时间 */
+    private LocalDateTime createAt;
+
+    /** 下级行政区列表 */
+    private transient List<PulDistrict> districts;
+}
---
+++ src/main/java/top/pullulate/utils/DistrictUtils.java
@@ -0,0 +1,76 @@
+package top.pullulate.utils;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import top.pullulate.system.entity.PulDistrict;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @功能描述:   地区工具类
+ * @Author: xuyong
+ * @Date: 2020/7/30 21:49
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+@Slf4j
+@Component
+public class DistrictUtils {
+
+    @Value("${gaode.key}")
+    private String key;
+
+    @Value("${gaode.districtUrl}")
+    private String districtUrl;
+
+    private int timeOut = 10000;
+
+    /**
+     * 获取所有的地区信息
+     *
+     * @return
+     */
+    public List<PulDistrict> getALLDistrictInfo() {
+        log.info(">>>>>>>>>>>>>>>>>>>>>>>> 请求高德平台获取地区信息");
+        String result = HttpUtil.get(districtUrl + "&subdistrict=3", timeOut);
+        if (StrUtil.isBlank(result)) {
+            log.info("<<<<<<<<<<<<<<<<<<<<<<<< 高德平台未响应数据或请求超时，请求结束");
+            return null;
+        }
+        JSONObject json = JSONUtil.parseObj(result);
+        log.info("高德平台响应状态码：{}，响应消息：{}", json.getStr("status"), json.getStr("info"));
+        if (json.getStr("status").equals("1")) {
+            JSONArray jsonArray = json.getJSONArray("districts");
+            List<PulDistrict> districts = buildDistricts(jsonArray.toList(PulDistrict.class), "0");
+            log.info("<<<<<<<<<<<<<<<<<<<<<<<< 成功处理响应数据，请求结束");
+            return districts;
+        } else {
+            log.info("<<<<<<<<<<<<<<<<<<<<<<<< 未获取到响应数据，请求结束");
+            return new ArrayList<PulDistrict>();
+        }
+    }
+
+    private List<PulDistrict> buildDistricts(List<PulDistrict> districts, String parentId) {
+        LocalDateTime now = LocalDateTime.now();
+        districts.forEach(district -> {
+            district.setDistrictId(IdUtil.fastSimpleUUID());
+            district.setParentId(parentId);
+            district.setCreateAt(now);
+            if (CollectionUtil.isNotEmpty(district.getDistricts())) {
+                List<PulDistrict> children = buildDistricts(district.getDistricts(), district.getDistrictId());
+                district.setDistricts(children);
+            }
+        });
+        return districts;
+    }
+}
