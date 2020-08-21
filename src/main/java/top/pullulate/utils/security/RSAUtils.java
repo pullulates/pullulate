35ca8f0544ee29a7b53156c29c96d2842ad62dca
pullulates <t1heluosh1@163.com>
2020-08-21 15:56:27 +0800

    pullulate V1.0封版

--- src/main/java/top/pullulate/utils/security/RSAUtils.java
+++ src/main/java/top/pullulate/utils/security/RSAUtils.java
@@ -23,14 +23,6 @@ import java.security.spec.X509EncodedKeySpec;
 public class RSAUtils {
 
     public static void main(String[] args) {
-        log.info("AES.generateRandomKey()：{}", AES.generateRandomKey());
-        String url = AES.encrypt("pullulate", "53c338fb709e9906");
-        String username = AES.encrypt("ffbc230213efaaccbea80d47e0b818ee", "53c338fb709e9906");
-        log.info("url：{}", url);
-        log.info("username：{}", username);
-        log.info("密钥原文：{}", "pullulate");
-        log.info("密钥密文：{}", Base64.encode("pullulate"));
-        log.info("密钥解密：{}", Base64.decodeStr(Base64.encode("pullulate")));
     }
 
     /**
