4f1a9ee4807a4301b2552c7354359811b0b8ad21
pullulates <t1heluosh1@163.com>
2020-08-26 10:50:51 +0800

    mp代码生成功能

---
+++ src/main/java/top/pullulate/core/mybatisplus/CodeGeneration.java
@@ -0,0 +1,117 @@
+package top.pullulate.core.mybatisplus;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.FileType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @功能描述:   代码生成
+ * @Author: xuyong
+ * @Date: 2020/8/26 10:28
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+public class CodeGeneration {
+
+    private static final String moduleName = "business";
+    private static final String parentPackageName = "top.pullulate";
+    private static final String tables = "biz_banner";
+    private static final String table_preffix = "biz_";
+
+    public static void main(String[] args) {
+        AutoGenerator generator = new AutoGenerator();
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("xuyong");
+        gc.setOpen(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        generator.setGlobalConfig(gc);
+        // 数据源配置
+        DataSourceConfig dataSource = new DataSourceConfig();
+        dataSource.setUrl("jdbc:mysql://");
+        dataSource.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        generator.setDataSource(dataSource);
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(moduleName);
+        pc.setParent(parentPackageName);
+        generator.setPackageInfo(pc);
+        // 自定义配置
+        InjectionConfig injectionConfig = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        // 如果模板引擎是 velocity
+        String templatePath = "/templates/mapper.xml.vm";
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果Entity设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mybatis/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        injectionConfig.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir(filePath);
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        injectionConfig.setFileOutConfigList(focList);
+        generator.setCfg(injectionConfig);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+        templateConfig.setXml(null);
+        generator.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+        // strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        //strategy.setSuperEntityColumns("id");
+        strategy.setInclude(tables.split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(table_preffix);
+        generator.setStrategy(strategy);
+        generator.setTemplateEngine(new VelocityTemplateEngine());
+        generator.execute();
+    }
+}
