2c23818c91fe2e208ad7ac6fbbd7c69b69a1832d
pullulates <t1heluosh1@163.com>
2020-09-01 17:52:47 +0800

    微信公众号功能

---
+++ src/main/java/top/pullulate/web/controller/wechat/WechatOfficialAccountController.java
@@ -0,0 +1,22 @@
+package top.pullulate.web.controller.wechat;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @功能描述:   微信公众号前端控制器
+ * @Author: pullulate
+ * @Date: 2020年9月1日17:36:53
+ * @CopyRight: pullulate
+ * @GitHub: https://github.com/pullulate
+ * @Gitee: https://gitee.com/pullulate
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/wechat/official-account")
+public class WechatOfficialAccountController {
+
+}
+
---
+++ src/main/java/top/pullulate/web/controller/wechat/WechatVerifyServerController.java
@@ -0,0 +1,17 @@
+package top.pullulate.web.controller.wechat;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @功能描述:   校验服务器配置，由微信发起
+ * @Author: xuyong
+ * @Date: 2020/9/1 17:50
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+@RestController
+@RequestMapping("/wechat/verify-server")
+public class WechatVerifyServerController {
+}
