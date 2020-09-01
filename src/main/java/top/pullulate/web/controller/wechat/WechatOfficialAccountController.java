e4e23e3646bb1d55f7c256869b2b8708e5f89aef
pullulates <t1heluosh1@163.com>
2020-09-01 17:58:49 +0800

    微信公众号功能

--- src/main/java/top/pullulate/web/controller/wechat/WechatOfficialAccountController.java
+++ src/main/java/top/pullulate/web/controller/wechat/WechatOfficialAccountController.java
@@ -2,8 +2,11 @@ package top.pullulate.web.controller.wechat;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.pullulate.web.data.dto.response.P;
 
 /**
  * @功能描述:   微信公众号前端控制器
@@ -17,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/wechat/official-account")
 public class WechatOfficialAccountController {
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('wechat.official.account.query')")
+    public P getOfficialAccountList() {
+        return null;
+    }
 
 }
 
