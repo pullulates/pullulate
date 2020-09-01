6bb36b7d187b990fc4ec035391702d1adf0c424b
pullulates <t1heluosh1@163.com>
2020-09-01 18:07:38 +0800

    微信公众号功能

---
+++ src/main/java/top/pullulate/web/data/viewvo/wechat/WechatOfficialAccountViewVo.java
@@ -0,0 +1,15 @@
+package top.pullulate.web.data.viewvo.wechat;
+
+import lombok.Data;
+
+/**
+ * @功能描述:   微信公众号视图类
+ * @Author: xuyong
+ * @Date: 2020/9/1 18:07
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+@Data
+public class WechatOfficialAccountViewVo {
+}
---
+++ src/main/java/top/pullulate/web/data/vo/wechat/WechatOfficialAccountVo.java
@@ -0,0 +1,15 @@
+package top.pullulate.web.data.vo.wechat;
+
+import lombok.Data;
+
+/**
+ * @功能描述:   微信公众号参数接收类
+ * @Author: xuyong
+ * @Date: 2020/9/1 18:06
+ * @CopyRight: pullulates
+ * @GitHub: https://github.com/pullulates
+ * @Gitee: https://gitee.com/pullulates
+ */
+@Data
+public class WechatOfficialAccountVo {
+}
